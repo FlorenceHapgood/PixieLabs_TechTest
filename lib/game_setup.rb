2e3a506be0028f00c7dbcd02318dab5abf237b73
Florence Hapgood <flohapgood@icloud.com>
2018-12-12 15:51:10 +0000

    num_cards method created
    To ask how many cards each player should be dealt. Private method save_cards_input created to save the iput to cards_input

--- lib/game_setup.rb
+++ lib/game_setup.rb
@@ -1,16 +1,19 @@
 class Game_Setup
 
-attr_reader :player_input
-
   def initialize
-    @player_input = "User_input"
+    @player_input = 'User_input'
+    @cards_input = 'Num_input'
   end
 
   def num_players
-    puts "Welcome! How many are playing?"
+    puts 'Welcome! How many are playing?'
     save_player_input
   end
 
+  def num_cards
+    puts 'How many cards should each player be dealt?'
+    save_cards_input
+  end
 
   private
 
@@ -18,5 +21,8 @@ attr_reader :player_input
     @player_input = gets.chomp.to_i
   end
 
+  def save_cards_input
+    @cards_input = gets.chomp.to_i
+  end
 
 end
--- spec/game_setup_spec.rb
+++ spec/game_setup_spec.rb
@@ -4,11 +4,18 @@ describe Game_Setup do
 
   let(:game_setup) { described_class.new }
 
-  describe "#num_players" do
-    it "asks the user for the number of players" do
+  describe '#num_players' do
+    it 'asks the user for the number of players' do
       expect { game_setup.num_players }.to output("Welcome! How many are playing?\n").to_stdout
     end
-
   end
+
+  describe '#num_cards' do
+    it 'asks the user for the number of cards to be dealt to each player' do
+      expect { game_setup.num_cards }.to output("How many cards should each player be dealt?\n").to_stdout
+    end
+  end
+
+
 
 end
