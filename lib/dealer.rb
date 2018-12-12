653e91121550f258469750068b1346185c8c98f3
Florence Hapgood <flohapgood@icloud.com>
2018-12-12 20:25:05 +0000

    Deal method deals the cards
    Deck simplified with just numbers for the moment. Record created of each player's hand. Tests not done yet as this might be a private method

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -3,32 +3,26 @@ class Dealer
   attr_reader :deck
 
   def initialize
-    @deck = [{Hearts: 1}, {Diamonds: 1}, {Spades: 1}, {Clubs: 1},
-           {Hearts: 2}, {Diamonds: 2}, {Spades: 2}, {Clubs: 2},
-           {Hearts: 3}, {Diamonds: 3}, {Spades: 3}, {Clubs: 3},
-           {Hearts: 4}, {Diamonds: 4}, {Spades: 4}, {Clubs: 4},
-           {Hearts: 5}, {Diamonds: 5}, {Spades: 5}, {Clubs: 5},
-           {Hearts: 6}, {Diamonds: 6}, {Spades: 6}, {Clubs: 6},
-           {Hearts: 7}, {Diamonds: 7}, {Spades: 7}, {Clubs: 7},
-           {Hearts: 8}, {Diamonds: 8}, {Spades: 8}, {Clubs: 8},
-           {Hearts: 9}, {Diamonds: 9}, {Spades: 9}, {Clubs: 9},
-           {Hearts: 10}, {Diamonds: 10}, {Spades: 10}, {Clubs: 10},
-           {Hearts: 11}, {Diamonds: 11}, {Spades: 11}, {Clubs: 11},
-           {Hearts: 12}, {Diamonds: 12}, {Spades: 12}, {Clubs: 12},
-           {Hearts: 13}, {Diamonds: 13}, {Spades: 13}, {Clubs: 13},
-           ]
-    @hands = [{}]
+
+    @deck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6,
+          6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11,
+          11, 12, 12, 12, 12, 12, 13, 13, 13, 13]
+    @record = []
   end
 
   def deal(players, cards)
     deck.shuffle
-   num_players_counter = 0
+    num_players_counter = 0
     while num_players_counter != players
-      x = @deck.sample
-      puts x
-      num_players_counter += 1
+       hand = []
+       cards.times { hand << @deck.sample } #Need to remove the cards.
+       @record << { "Player number #{ num_players_counter + 1 }" => hand}
+       num_players_counter += 1
     end
 
+    print @record
   end
+
+
 
 end
--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -4,10 +4,4 @@ describe Dealer do
 
   let(:dealer) { described_class.new }
 
-  describe "#deal" do
-    it "#deals the cards" do
-      expect(dealer.deal("5", "4")).to eq([{}])
-    end
-
-  end
 end
